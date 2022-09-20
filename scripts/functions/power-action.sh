02ebcf7660c3c041747c01113ceb5657c0c5f841
Artem S <admin@tenorium.net>
2022-09-20 22:10:16 +0300

    Script and apache config fix

--- scripts/functions/power-action.sh
+++ scripts/functions/power-action.sh
@@ -2,11 +2,15 @@
 
 case "$1" in
 "up")
-docker-compose -p tenorium -f ~/workspace/tenorium/docker-compose.yml up
+echo "Starting containers.."
+docker-compose -p tenorium -f ~/workspace/tenorium/docker-compose.yml up -d
 ;;
 "down")
+echo "Stopping containers..."
 docker-compose -p tenorium -f ~/workspace/tenorium/docker-compose.yml down
 ;;
 "restart")
+echo "Restarting containers..."
 docker-compose -p tenorium -f ~/workspace/tenorium/docker-compose.yml restart
+;;
 esac
