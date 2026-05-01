9cd771ed45e3dfda9c2f9258eef753f5c45d8bdb
HACKERPRO17 <sartyom532@gmail.com>
2026-05-01 21:33:44 +0300

    fix: Replaced copy inside of container with copy outside

--- scripts/functions/setup.sh
+++ scripts/functions/setup.sh
@@ -22,7 +22,7 @@ MAIN_SCRIPT_PATH="$( dirname -- "$FUNCTIONS_PATH";)/main.sh"
 $MAIN_SCRIPT_PATH clone
 $MAIN_SCRIPT_PATH mkcert {{projectCommand}}.tenorium.local
 $MAIN_SCRIPT_PATH up
-$MAIN_SCRIPT_PATH docker exec php8.4 -c "cp .env.docker .env"
+cp ~/workspace/smarthead/www/site/.env.docker ~/workspace/smarthead/www/site/.env
 $MAIN_SCRIPT_PATH composer install -o --no-interaction
 $MAIN_SCRIPT_PATH artisan migrate --seed --no-interaction
 $MAIN_SCRIPT_PATH build
